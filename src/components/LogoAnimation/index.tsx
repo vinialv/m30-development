8fc8ba044ad3144524a2ed4224ef34ca3719ecd4
Vinicius Oliveira Alves <vini.alv15@hotmail.com>
2023-12-09 23:18:55 -0300

    Corrigindo o tempo da animação

--- src/components/LogoAnimation/index.tsx
+++ src/components/LogoAnimation/index.tsx
@@ -6,22 +6,37 @@ import Arquitetura30SVG from '../Arquitetura30SVG'
 import * as S from 'components/LogoAnimation/styles'
 
 export function LogoAnimation() {
-  const [logoColor, setLogoColor] = useState('none')
+  const [logoColor, setLogoColor] = useState(false)
 
   const numberElements = 9
   const elementsArray = Array.from({ length: numberElements }).map(
     (item, index) => (
-      <M_SVG key={index} fill={logoColor} stroke={'#fff'} strokeWidth={'5'}/>
+      <M_SVG
+        key={index}
+        fill={logoColor ? '#fff' : 'none'}
+        stroke={'#fff'}
+        strokeWidth={'5'}
+      />
     )
   )
 
   useEffect(() => {
+    const tempoInicial = new Date().getTime()
+
     const changeM_Color = setTimeout(() => {
-      setLogoColor('#fff')
+      setLogoColor(true)
     }, 3250)
 
     return () => {
+      const tempoFinal = new Date().getTime()
+      const tempoTotal = tempoFinal - tempoInicial
+
+      const tempoRestante = Math.max(0, 3250 - tempoTotal)
+
       clearTimeout(changeM_Color)
+      setTimeout(() => {
+        setLogoColor(true)
+      }, tempoRestante)
     }
   }, [])
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import * as S from '@/styles'
 import M30SVG from '../components/M30SVG'
 import { useEffect, useState } from 'react'
+import { StyleSheetManager } from 'styled-components'
 import { LogoAnimation } from '@/components/LogoAnimation'
 import { WhatsappLogo, InstagramLogo, EnvelopeSimple } from 'phosphor-react'
 
@@ -18,27 +19,29 @@ export default function Index() {
   }, [])
 
   return (
-    <S.Home splashScreen={splashScreen}>
-      <S.Background />
-      {splashScreen && <LogoAnimation />}
-      <header>
-        <M30SVG fill={'white'} />
-      </header>
-      <main>Nosso site está em desenvolvimento.</main>
-      <footer>
-        <p>Entre em contato conosco!</p>
-        <nav className="Contatos">
-          <Link href="https://wa.me/5516982149970" target="_blank">
-            <WhatsappLogo size={32} weight="thin" />
-          </Link>
-          <Link href="https://www.instagram.com/m30.arq/" target="_blank">
-            <InstagramLogo size={32} weight="thin" />
-          </Link>
-          <Link href="mailto:muriloalves@m30.arq.br" target="_blank">
-            <EnvelopeSimple size={32} weight="thin" />
-          </Link>
-        </nav>
-      </footer>
-    </S.Home>
+    <StyleSheetManager shouldForwardProp={(prop) => prop !== 'splashScreen'}>
+      <S.Home splashScreen={splashScreen}>
+        <S.Background />
+        {splashScreen && <LogoAnimation />}
+        <header>
+          <M30SVG fill={'white'} />
+        </header>
+        <main>Nosso site está em desenvolvimento.</main>
+        <footer>
+          <p>Entre em contato conosco!</p>
+          <nav className="Contatos">
+            <Link href="https://wa.me/5516982149970" target="_blank">
+              <WhatsappLogo size={32} weight="thin" />
+            </Link>
+            <Link href="https://www.instagram.com/m30.arq/" target="_blank">
+              <InstagramLogo size={32} weight="thin" />
+            </Link>
+            <Link href="mailto:muriloalves@m30.arq.br" target="_blank">
+              <EnvelopeSimple size={32} weight="thin" />
+            </Link>
+          </nav>
+        </footer>
+      </S.Home>
+    </StyleSheetManager>
   )
 }
